54bb861bae819a95c389e1db75f3a0723880f71f
umutergene <umut_ergene_67@hotmail.com>
2020-01-08 15:06:21 +0300

    delete

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Theme Carbon',
-    description: 'A Gatsby theme for the carbon design system',
-    keywords: 'gatsby,theme,carbon',
-  },
-  plugins: ['gatsby-theme-carbon'],
-};
